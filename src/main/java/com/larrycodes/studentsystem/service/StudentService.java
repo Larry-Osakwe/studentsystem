23f8eb9a5d4fb88d546e033bada9078056154467
Larry Osakwe <larryosak@gmail.com>
2023-05-29 02:15:36 -0700

    Add post mapping

--- src/main/java/com/larrycodes/studentsystem/service/StudentService.java
+++ src/main/java/com/larrycodes/studentsystem/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.larrycodes.studentsystem.service;
 
+import com.larrycodes.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
--- src/main/java/com/larrycodes/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/larrycodes/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,17 @@
 package com.larrycodes.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.larrycodes.studentsystem.model.Student;
+import com.larrycodes.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
