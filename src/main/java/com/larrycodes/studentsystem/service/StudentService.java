814e622c8b02a2ebc109b27658362188904dd878
Larry Osakwe <larryosak@gmail.com>
2023-05-30 20:50:32 -0700

    Add get all students endpoint

--- src/main/java/com/larrycodes/studentsystem/service/StudentService.java
+++ src/main/java/com/larrycodes/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.larrycodes.studentsystem.service;
 
 import com.larrycodes.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
